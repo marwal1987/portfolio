83ac603f7899b4b21c3e30b4e98c51eecadc503d
marwal1987 <marwal1987@gmail.com>
2024-02-16 16:15:20 +0100

    moved components import to index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import "@/styles/globals.css";
+import { useEffect } from "react";
+import { ThemeProvider, useTheme } from "../contexts/ThemeContext";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+function MyApp({ Component, pageProps }) {
+  const { state } = useTheme();
+
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", state.theme === "dark");
+  }, [state.theme]);
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default ({ Component, pageProps }) => (
+  <ThemeProvider>
+    <MyApp Component={Component} pageProps={pageProps} />
+  </ThemeProvider>
+);
